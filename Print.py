024ea78b9eaa4d7389830467bb3dc71d31c0063f
David DeMar <dfdemar@gmail.com>
2012-04-23 00:41:28 -0400

    moved classes to Statement module

--- Print.py
+++
@@ -1,7 +0,0 @@
-class Print(object):
-
-    def __init__(self, tokens = []):
-        super(Print, self).__init__(tokens)
-
-    def execute(self, skip):
-        
--- VariableException.py
+++
@@ -1,5 +0,0 @@
-class Variable(Exception):
-
-    def __init__(self, exception):
-        super.__init__(exception)
-        
